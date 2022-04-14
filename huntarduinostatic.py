ffcd007e71025f28d0a0b398fdf900ad59a8b7b5
Tony Caivano <italiancaivano@gmail.com>
2022-04-14 11:33:34 -0400

    Change discard var name. Add new Hunt file for variable static encounters (berries, sand piles, headbutt, etc)

--- huntarduinostatic.py
+++ huntarduinostatic.py
@@ -22,10 +22,8 @@ arduino = serial.Serial(port=shinyHuntUtils.findArduinoCOM(), baudrate=345600, t
 
 alg = sys.argv[1]
 commandItems, delays = shinyHuntUtils.readAndParseCommands(alg)
-alg2 = sys.argv[2]
-commandItems2, delays2 = shinyHuntUtils.readAndParseCommands(alg2)
 
-attempts = int(sys.argv[3])
+attempts = int(sys.argv[2])
 form = app.window(title_re='mGBA - ')
 
 # capture first coord to test
@@ -34,25 +32,19 @@ pos, _ = shinyHuntUtils.capturePosition("upper left")
 # capture second coord to test
 pos2, _ = shinyHuntUtils.capturePosition("bottom right")
 
-# capture third coord to anchor
-pos3, initialPixels3 = shinyHuntUtils.capturePosition("anchor")
-
 shinyHuntUtils.printCurrentProgress(attempts)
 form.send_keystrokes("]")
 time.sleep(0.75)
 
 found = False
-reset = True
 w = pos2[0]-pos[0]
 h = pos2[1]-pos[1]
 r = (pos[0], pos[1], w, h)
 while(1):
     time.sleep(0.75)
     attempts = attempts + 1
-    if reset == True:
-        shinyHuntUtils.writeCommands(arduino, commandItems, delays)
-    else:
-        shinyHuntUtils.writeCommands(arduino, commandItems2, delays2)
+    
+    shinyHuntUtils.writeCommands(arduino, commandItems, delays)
 
     # try to capture our shiny appearing
     screenshots = []
@@ -73,35 +65,8 @@ while(1):
     if (found):
         break
 
-    pixels = pyautogui.screenshot().load()
-    if ((pixels[pos3[0], pos3[1]] != initialPixels3[pos3[0], pos3[1]])):
-        form.send_keystrokes("]")
-        attempts = attempts - 1
-        reset = True
-    else:
-        reset = False
-    
-    # run from the fight and try again
-    if reset == False:
-        # weather - todo add if
-        # time.sleep(0.5)
-        # shinyHuntUtils.writeToDeviceAndSleep(arduino, "a", 0.5)
-
-        shinyHuntUtils.writeToDeviceAndSleep(arduino, "a", 0.25)
-        shinyHuntUtils.writeToDeviceAndSleep(arduino, "down", 0.25)
-        shinyHuntUtils.writeToDeviceAndSleep(arduino, "down", 0.25)
-        shinyHuntUtils.writeToDeviceAndSleep(arduino, "right", 0.25)
-        shinyHuntUtils.writeToDeviceAndSleep(arduino, "a", 0.5)
-        shinyHuntUtils.writeToDeviceAndSleep(arduino, "a", 1.5)
-        
-        # check to see if we were blocked from running
-        screenshot = pyautogui.screenshot()
-        pixels = screenshot.load()
-        if ((pixels[pos3[0], pos3[1]] == initialPixels3[pos3[0], pos3[1]])):
-            form.send_keystrokes("]")
-            reset = True
-        else:
-            reset = False
+    form.send_keystrokes("]")
+    time.sleep(0.75)
 
     shinyHuntUtils.printCurrentProgress(attempts)
 
